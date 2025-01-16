4316d6e5c5ca5a445298f76a20a5b7202c1f2560
Hussein Playz <houseinplayz@gmail.com>
2025-01-16 18:10:47 +0200

    Delete sources directory

--- sources/luna_launcher/packaging/linux/build.sh
+++
@@ -1,3 +0,0 @@
-# Deb packaging
-docker build -t lunalauncher/atlauncher-packaging-linux-deb -f deb/Dockerfile .
-docker run --rm -i -v $PWD/out:/work/out -w /work/out atlauncher/atlauncher-packaging-linux-deb dpkg-deb --build ../lunalauncher luna-launcher-1.4-1.deb
